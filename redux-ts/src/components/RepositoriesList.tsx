4ec957c452f56589a9813e3625d7ddd3fe2ec8b7
harshal-ganbote <harshal.ganbote@outlook.com>
2024-01-30 14:24:51 +0530

    repositories done

---
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -0,0 +1,21 @@
+import { FormEvent, useState } from "react";
+
+const RepositoriesList: React.FC = () => {
+
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={e => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
